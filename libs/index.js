04f9f336a2085ecdd4a4c19f81e6a7cc5ac18b50
wryi <cherlton.millette@gmail.com>
2014-08-01 07:43:45 -0500

    Changes to server start file

--- libs/index.js
+++ libs/index.js
@@ -1,2 +1,17 @@
 // First thing we need to do is load the application
 // configuration files
+
+var express = require('express')
+var app = express()
+
+app.get('/', function(req, res){
+	res.send('hello world')
+})
+
+
+
+module.exports = {
+	start: function (){
+		app.listen(3000)
+	}
+}
