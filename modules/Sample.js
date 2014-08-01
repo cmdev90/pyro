f746563cf33a9e448f0cf033fa021d6e11bcb2cc
wryi <cherlton.millette@gmail.com>
2014-07-31 19:28:31 -0500

    Added sample module to project

---
+++ modules/Sample.js
@@ -0,0 +1,7 @@
+// Sample module file.
+
+module.exports = function () { // You can pass dependencies to module!
+
+	return null // module must return a js object describing 
+				 // all public methods.
+}
