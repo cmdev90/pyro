e5acab51b53e500e0ce9ac1931b0c6a5aef9dd44
wryi <cherlton.millette@gmail.com>
2014-08-08 08:59:04 -0500

    Updates to the module object structure with comments

--- modules/Sample.js
+++ modules/Sample.js
@@ -1,7 +1,41 @@
 // Sample module file.
+// Modules will be made accesable via the global module
+// object.
 
-module.exports = function () { // You can pass dependencies to module!
+// Modules invoked by the framework will be passed the 
+// req and res object parameters. When modules are invoked manually
+// you can pass your own parameters directly to them. Paramters defined
+// in the function signature will be undefined when invoked
+// by the framework.
 
-	return null // module must return a js object describing 
-				 // all public methods.
-}
+module.exports = function (req, res) {
+	 // Write all private variables here;
+	 var user = req.user
+
+	 // You can describe private functions here as well.
+	 var privateFunction = function (options){
+	 	return console.log("options passed", options)
+	 }
+
+	 // return a POJSO describing the public functions of this 
+	 // object.
+	return {
+		// public functions can take its own parameters.
+		publicFunction: function (param, callback) { 
+			// We can call private functions here!
+			privateFunction({message: 'this is a call from private'})
+
+			// Because of cloures you have access to the
+			// req and res objects!
+			// if (req && res) {
+				console.log("req", req)
+				console.log("res", res)
+			// }
+			// else {
+				// You are able to choose context of execution by either returning to
+				// the res object or passing the result directly back.
+				return callback(param)
+			// }
+		}
+	}
+} // never self-invoke this function space. You will break things...
